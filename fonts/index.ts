63c6a8a6bfcf78454d657346d35a19ea55ae5e5e
FHRQ <xt9950@rimac.com.pe>
2025-05-29 18:40:11 -0500

    [xt9950] Subiendo template (#1)

---
+++ fonts/index.ts
@@ -0,0 +1,71 @@
+import localFont from "next/font/local";
+
+export const hyundaiHeadLight = localFont({
+  src: "./headOffice/HyundaiSansHeadOffice-Light.woff2",
+  variable: "--font-hyundai-head-light",
+  weight: "300",
+  style: "normal",
+});
+
+export const hyundaiHeadRegular = localFont({
+  src: "./headOffice/HyundaiSansHeadOffice-Regular.woff2",
+  variable: "--font-hyundai-head-regular",
+  weight: "400",
+  style: "normal",
+});
+
+export const hyundaiHeadMedium = localFont({
+  src: "./headOffice/HyundaiSansHeadOffice-Medium.woff2",
+  variable: "--font-hyundai-head-medium",
+  weight: "500",
+  style: "normal",
+});
+
+export const hyundaiHeadbold = localFont({
+  src: "./headOffice/HyundaiSansHeadOffice-Bold.woff2",
+  variable: "--font-hyundai-head-bold",
+});
+
+// Text Office Normal
+export const hyundaiTextRegular = localFont({
+  src: "./textOffice/HyundaiSansTextOffice-Regular.woff2",
+  variable: "--font-hyundai-text-regular",
+  weight: "400",
+  style: "normal",
+});
+
+export const hyundaiTextMedium = localFont({
+  src: "./textOffice/HyundaiSansTextOffice-Medium.woff2",
+  variable: "--font-hyundai-text-medium",
+  weight: "500",
+  style: "normal",
+});
+
+export const hyundaiTextBold = localFont({
+  src: "./textOffice/HyundaiSansTextOffice-Bold.woff2",
+  variable: "--font-hyundai-text-bold",
+  weight: "700",
+  style: "normal",
+});
+
+// Text Office Italic
+export const hyundaiTextItalicRegular = localFont({
+  src: "./textOffice/HyundaiSansTextOffice-Italic.woff2",
+  variable: "--font-hyundai-text-italic-regular",
+  weight: "400",
+  style: "italic",
+});
+
+export const hyundaiTextItalicMedium = localFont({
+  src: "./textOffice/HyundaiSansTextOffice-MediumItalic.woff2",
+  variable: "--font-hyundai-text-italic-medium",
+  weight: "500",
+  style: "italic",
+});
+
+export const hyundaiTextItalicBold = localFont({
+  src: "./textOffice/HyundaiSansTextOffice-BoldItalic.woff2",
+  variable: "--font-hyundai-text-italic-bold",
+  weight: "700",
+  style: "italic",
+});
